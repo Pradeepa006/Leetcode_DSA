548ff254665d7bbf730dfbc88cc7a1fd8abe011a
Pradeepa006 <155996905+Pradeepa006@users.noreply.github.com>
2026-06-16 22:26:32 +0530

    Time: 4 ms (76.47%), Space: 55.1 MB (75%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -1,39 +1,22 @@
 class Solution {
-    public static void reverse (StringBuilder sb) {
-        // int n = sb.length();
-        // int i = 0 ; 
-        // int j = n - 1 ;
-        // while(i < j) {
-        //     char temp = sb.chsrAt(i);
-
-        // }
-        sb.reverse();
-    }
-    public static void duplicate (StringBuilder sb) {
-        sb = sb.append(sb);
-    }
-    public static void removeLast(StringBuilder sb) {
-        int n = sb.length();
-        sb.deleteCharAt(n - 1);
-    }
     public String processStr(String s) {
         StringBuilder res = new StringBuilder();
-        int n = s.length();
-        for(int i = 0 ; i < n ; i++) {
+
+        for(int i=0; i<s.length(); i++){
             char ch = s.charAt(i);
-            if(ch >= 'a' && ch <= 'z') {
+            if(ch == '#'){
+                res.append(res);
+            }else if(ch == '%'){
+                res.reverse();
+            }else if(ch == '*'){
+                if(res.length() > 0){
+                    res.deleteCharAt(res.length() - 1);
+                }
+            }else{
                 res.append(ch);
-                continue;            
-            }
-            if(res.length() >= 1) {
-            if(ch == '#')
-                duplicate(res);
-            else if (ch == '%')
-                reverse(res);
-            else
-                removeLast(res);
             }
         }
         return res.toString();
+
     }
 }
