dee9465653dee6abd219c55b431f3109f6c1b836
Pradeepa006 <155996905+Pradeepa006@users.noreply.github.com>
2026-07-06 10:27:38 +0530

    Time: 1 ms (99.74%), Space: 46.4 MB (76.82%) - LeetHub

---
+++ 3936-minimum-swaps-to-move-zeros-to-end/3936-minimum-swaps-to-move-zeros-to-end.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minimumSwaps(int[] nums) {
+        int n = nums.length;
+        int i = 0 ;
+        int j = n - 1 ;
+        int count = 0 ;
+        while(i < j) {
+            if(nums[i] == 0 && nums[j] != 0) {
+                count++;
+                j--;
+            }
+            else if((nums[i] != 0 && nums[j] == 0) || (nums[i] == 0 && nums[j] == 0)) {
+                j--;
+                continue;
+            }
+            i++;
+        }
+        return count;
+    }
+}
