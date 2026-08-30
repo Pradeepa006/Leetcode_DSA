f7af98343414a8cff0e338e5cd403cb8c09203af
Pradeepa S <155996905+Pradeepa006@users.noreply.github.com>
2026-08-30 21:40:07 +0530

    Time: 4 ms (10.73%), Space: 86.5 MB (89.09%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
@@ -3,7 +3,6 @@ class Solution {
         int n = nums.length;
         int left = 0;
         int right = 0;
-        
         for (int i = 1; i < n; i++) {
             if (nums[i] < nums[left])
                 left = i;
@@ -11,15 +10,12 @@ class Solution {
             if (nums[i] > nums[right])
                 right = i;
         }
-        
         if (left < right) {
             int temp = left;
             left = right;
             right = temp;
-        }
-            
+        }  
         int ans = n;
-        
         for (int i = 0; i <= n; i++) {
             int extra = 0;
             
